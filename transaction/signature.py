0c9cb14d19e99228c491c2d00a10f186fb1c993d
stewartfortier <30375163+stewartfortier@users.noreply.github.com>
2018-02-22 17:49:49 -0800

    So, signatures are deceptively hard...restarting.

--- transaction/signature.py
+++ transaction/signature.py
@@ -1,39 +1,41 @@
-import hashlib
-from random import choice
-from string import ascii_letters, digits
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Signature
+from Crypto import Random
 
 class Signature(object):
     def generate_keys(self):
-
-        # choose arbitrary string length + character set
-        string_length = 64
-        chars = ascii_letters + digits
-
-        # generate random strings
-        public_key = ''.join([choice(chars) for n in range(string_length)])
-        secret_key = ''.join([choice(chars) for n in range(string_length)])
-
-        # hash strings
-        h1, h2 = hashlib.sha256(), hashlib.sha256(),
-        h1.update(public_key.encode('utf-8'))
-        h2.update(secret_key.encode('utf-8'))
-        public_key, secret_key = h1.hexdigest(), h2.hexdigest()
-
-        return public_key, secret_key
+        # return secret_key, public_key
+        pass
 
     def sign(message, secret_key):
-        message = str(message) # ensure message is a string
-        h = hashlib.sha256()
-        h.update((message + secret_key).encode('utf-8'))
-        return h.hexdigest()
+        # should do: secret_key.encrypt(message)
+        pass
 
     def verify(public_key, message, signature):
-        # return isValid (Bool)
+        # should do: public_key.decrypt(signature) == message
         pass
 
 
 if __name__ == "__main__":
-    print("Generating public and private keys...")
-    pk, sk = Signature().generate_keys()
-    print("Public key: %s" % pk)
-    print("Secret key: %s" % sk)
+    # Experimenting with pycryptodome Library
+    # Using video: https://www.youtube.com/watch?v=OKg4PqD01Z0
+
+    random_generator = Random.new().read
+    keyPair = RSA.generate(1024, random_generator)
+    pubKey = keyPair.publickey()
+
+    data_to_sign = "This is a sample transaction".encode('utf-8')
+
+    hashA = SHA256.new(data_to_sign).digest()
+    signature = keyPair.sign(hashA, '')
+
+    print("Hash of transaction: %s" % hashA)
+    print("Signature: %s" % repr(signature))
+
+    data_to_verify = "This is a sample transaction".encode('utf-8')
+
+    if pubKey.verify(hashA, signature):
+        print("Signature is valid!")
+    else:
+        print("Signature is not valid!")
